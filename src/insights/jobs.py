31c1642ddd201e61bb5b44461e47813f64cabcbc
Larissa Perinoto <perinotolarissa@gmail.com>
2023-02-16 17:03:16 -0300

    feat: implementa função get_unique_job_types

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -17,21 +17,12 @@ def read(path: str) -> List[Dict]:
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    raise NotImplementedError
+    job_list = read(path)
+    list_unique_job_types = []
+    for job in job_list:
+        if job["job_type"] not in list_unique_job_types:
+            list_unique_job_types.append(job["job_type"])
+    return list_unique_job_types
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
