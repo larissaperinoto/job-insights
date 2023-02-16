a166ab96bb0599d02f7c545ef5bd6be4cb298dc5
Larissa Perinoto <perinotolarissa@gmail.com>
2023-02-16 17:37:33 -0300

    feat: implementa função get_unique_industries

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,22 +1,15 @@
 from typing import List, Dict
 
+import src.insights.jobs as jobs
+
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    file_list = jobs.read(path)
+    list_unique_industry = []
+    for job in file_list:
+        if job["industry"] not in list_unique_industry:
+            list_unique_industry.append(job["industry"])
+    return list_unique_industry
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
