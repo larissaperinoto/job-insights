090da3c851d231a3e45f11a77169a493384ccabb
Larissa Perinoto <perinotolarissa@gmail.com>
2023-02-16 18:10:20 -0300

    feat: insere função get_min_salary

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -6,29 +6,20 @@ import src.insights.jobs as jobs
 
 def get_max_salary(path: str) -> int:
     file_list = jobs.read(path)
-    list_max_salary = []
+    list_max_salaries = []
     for job in file_list:
         if job["max_salary"] and job["max_salary"] != 'invalid':
-            list_max_salary.append(int(job["max_salary"]))
-    return max(list_max_salary)
+            list_max_salaries.append(int(job["max_salary"]))
+    return max(list_max_salaries)
 
 
 def get_min_salary(path: str) -> int:
-    """Get the minimum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    raise NotImplementedError
+    file_list = jobs.read(path)
+    list_min_salaries = []
+    for job in file_list:
+        if job["min_salary"] and job["min_salary"] != 'invalid':
+            list_min_salaries.append(int(job["min_salary"]))
+    return min(list_min_salaries)
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
