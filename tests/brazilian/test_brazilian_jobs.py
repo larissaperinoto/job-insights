de02a377ab108f86c1a2c9228623e0b49158bc1a
Larissa Perinoto <perinotolarissa@gmail.com>
2023-02-17 17:37:37 -0300

    tests: implementa teste para a função read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,9 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    result = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    assert result[0] == {
+        "title": "Maquinista",
+        "salary": "2000", "type": "trainee"
+        }
