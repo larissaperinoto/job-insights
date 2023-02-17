1ce27a1ad558322f8aeec10e131d34748bb12bd9
Larissa Perinoto <perinotolarissa@gmail.com>
2023-02-17 16:30:51 -0300

    feat: insere função matches_salary_range

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -23,29 +23,15 @@ def get_min_salary(path: str) -> int:
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    raise NotImplementedError
+    try:
+        if int(job["min_salary"]) > int(job["max_salary"]):
+            raise ValueError
+        if int(job['min_salary']) <= int(salary) <= int(job['max_salary']):
+            return True
+        else:
+            return False
+    except (TypeError, KeyError):
+        raise ValueError
 
 
 def filter_by_salary_range(
